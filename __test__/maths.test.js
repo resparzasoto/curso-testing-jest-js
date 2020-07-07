ca2a44a302cd65b71b05d06695260cc0db0eda3f
resparzasoto <rubenesparzasoto95@gmail.com>
2020-07-07 01:38:55 -0600

    Creando nuestra primera prueba unitaria

---
+++ __test__/maths.test.js
@@ -0,0 +1,28 @@
+import { add, subtract, multiply, divide } from '../src/maths';
+
+describe('Mathematics calculations', () => {
+    test('OneMoreOneShouldBeTwo', () => {
+        expect(add(1, 1)).toBe(2);
+    });
+    test('OneMoreOneNotShouldBeThree', () => {
+        expect(add(1, 1)).not.toBe(3);
+    });
+    test('OneLessOneShouldBeZero', () => {
+        expect(subtract(1, 1)).toBe(0);
+    });
+    test('OneLessOneNotShouldBeOne', () => {
+        expect(subtract(1, 1)).not.toBe(1);
+    });
+    test('TwoForTwoShouldBeFour', () => {
+        expect(multiply(2, 2)).toBe(4);
+    });
+    test('TwoForTwoNotShouldBeThree', () => {
+        expect(multiply(2, 2)).not.toBe(3);
+    });
+    test('FourDivideInTwoShouldBeTwo', () => {
+        expect(divide(4, 2)).toBe(2);
+    });
+    test('FourDiveInTwoNotShouldBeThree', () => {
+        expect(divide(4, 2)).not.toBe(3);
+    });
+});
