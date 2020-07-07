641ee52d149cd318c6fb7c5d02339773e210f2f1
resparzasoto <rubenesparzasoto95@gmail.com>
2020-07-07 08:04:06 -0600

    Truthness / Verdadero

---
+++ __test__/true.test.js
@@ -0,0 +1,27 @@
+import { isTrue, isFalse, isNull, isUndefined } from '../src/true';
+
+describe('Compare truthiness', () => {
+    describe('Compare truthiness of null', () => {
+        test('NullShouldBeNull', () => {
+            expect(isNull()).toBeNull();
+        });
+    });
+    describe('Compare truthiness of true', () => {
+        test('TrueShouldBeTruthy', () => {
+            expect(isTrue()).toBeTruthy();
+        });
+    });
+    describe('Compare truthiness of false', () => {
+        test('FalseShouldBeFalsy', () => {
+            expect(isFalse()).toBeFalsy();
+        });
+        test('FalseNotShouldBeTruthy', () => {
+            expect(isFalse()).not.toBeTruthy();
+        });
+    });
+    describe('Compare truthiness of undefined', () => {
+        test('UndefinedShouldBeUndefined', () => {
+            expect(isUndefined()).toBeUndefined();
+        });
+    });
+});
---
+++ src/true.js
@@ -0,0 +1,7 @@
+export const isTrue = () => true;
+
+export const isFalse = () => false;
+
+export const isNull = () => null;
+
+export const isUndefined = () => undefined;
