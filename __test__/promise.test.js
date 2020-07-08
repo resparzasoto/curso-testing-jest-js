0e89598eb94b59d2582e272e2e3b58a294157666
resparzasoto <rubenesparzasoto95@gmail.com>
2020-07-08 06:49:05 -0600

    Reject y resolve

--- __test__/promise.test.js
+++ __test__/promise.test.js
@@ -1,11 +1,17 @@
 import { getDataFromApi } from '../src/promise';
 
 describe('Check promises', () => {
-    test('SendRequestToApi', done => {
+    test('CountShouldBeGreaterThan0', done => {
         getDataFromApi('https://rickandmortyapi.com/api/character')
             .then(data => {
                 expect(data.info.count).toBeGreaterThan(0);
                 done();
             });
+    });
+    test('ResolveShouldBeHi!', () => {
+        return expect(Promise.resolve('Hi!')).resolves.toBe('Hi!')
+    });
+    test('RejectShouldBeHi!', () => {
+        return expect(Promise.reject(new Error('Error'))).rejects.toThrow('Error');
     });
 });
